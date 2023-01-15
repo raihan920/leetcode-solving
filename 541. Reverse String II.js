19ecdd5e577547e1329aed5164d5416aadbb194b
Raihan Chowdhury <raihan92@rocketmail.com>
2023-01-15 15:18:29 +0600

    working with prob 541

---
+++ 541. Reverse String II.js
@@ -0,0 +1,13 @@
+var reverseStr = function(s, k) {
+    console.log(s);
+    console.log(k);
+
+    for(let i = 0; i<k; i++){
+        //
+    }
+};
+
+let s = "abcdefg";
+let k = 2;
+
+reverseStr(s, k);
